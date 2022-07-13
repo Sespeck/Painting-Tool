6ca2da08669dc8708a8c10e84f49665a4b893b49
Sespeck <jacfan17@gmail.com>
2022-07-13 06:21:33 -0400

    five color strokes available to paint on the canvas

--- app/src/main/java/seanwang/paint/MainActivity.kt
+++ app/src/main/java/seanwang/paint/MainActivity.kt
@@ -1,9 +1,14 @@
 package seanwang.paint
 
+import android.annotation.SuppressLint
+import android.content.res.Resources
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import seanwang.paint.PaintView.Companion.currentPaint
 import seanwang.paint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +19,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.blackPaint.setOnClickListener{}
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.blackPaint.setOnClickListener{
+            paintBrush.color = ContextCompat.getColor(this, R.color.black)
+            currentColor(paintBrush.color)
+        }
+        binding.redPaint.setOnClickListener{ paintBrush.color = ContextCompat.getColor(this, R.color.red); currentColor(paintBrush.color)}
+        binding.bluePaint.setOnClickListener{ paintBrush.color = ContextCompat.getColor(this, R.color.blue); currentColor(paintBrush.color)}
+        binding.yellowPaint.setOnClickListener{ paintBrush.color = ContextCompat.getColor(this, R.color.yellow); currentColor(paintBrush.color)}
+        binding.whitePaint.setOnClickListener{ paintBrush.color = ContextCompat.getColor(this, R.color.white); currentColor(paintBrush.color)}
+    }
+
+    private fun currentColor(color: Int){
+        currentPaint = color
+        path = Path()
     }
 }
